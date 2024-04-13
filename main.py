d3d0a511517cf721c3ea3a85831321efea77bd6b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-12 21:49:24 -0300

    versoes

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ salario = 4650.37
 msg = '%s tem %d e ganha R$%.2f' % (nome, idade, salario)
 print(msg)
 
+msg2 = '{:-^20s} tem {} anos e ganha R${:.2f}'.format(nome, idade, salario)
+print(msg2)
 
+
+msg3 = f'{nome:*^15s} tem {idade} anos e ganha R${salario:.1f}'
+print(msg3)
